19a0b9e4d5ebdae524d17f945f24eae280318ae2
DJ Ambrisco <dambrisco@gmail.com>
2023-07-20 07:28:46 +0000

    Remove unusued  function

--- util.go
+++ util.go
@@ -2,9 +2,7 @@ package main
 
 import (
 	"encoding/json"
-	"io"
 	"net/http"
-	"os"
 )
 
 type response struct {
@@ -47,16 +45,4 @@ func writeSuccess(w http.ResponseWriter, path string) (int, error) {
 		return w.Write([]byte{})
 	}
 	return w.Write(b)
-}
-
-func getSize(content io.Seeker) (int64, error) {
-	size, err := content.Seek(0, os.SEEK_END)
-	if err != nil {
-		return 0, err
-	}
-	_, err = content.Seek(0, io.SeekStart)
-	if err != nil {
-		return 0, err
-	}
-	return size, nil
 }
